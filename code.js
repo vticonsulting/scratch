d10570bae16ffbb27cdc970460c1a246f377cf17
Victor Tolbert <victor.tolbert@gmail.com>
2019-03-15 19:31:13 -0400

    add sample code file with inconsistent formatting

---
+++ code.js
@@ -0,0 +1,26 @@
+function sayHi(   name     ) {
+  const age = 10;
+}
+
+let age=12
+
+var things = ['cool', "doubleee!!!"     ];
+
+function foo(bar = "baz") {
+	return bar;
+}
+
+var color = "#FF0000" 
+
+let config = {
+  color: color,
+	data: function() {
+return {}
+}
+}
+
+
+let completedItems = items.filter(function(item) {
+	return item.isCompleted
+})
+
