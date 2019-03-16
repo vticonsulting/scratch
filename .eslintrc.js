d838e711f0011306885bf2af40391a84bb829a78
Victor Tolbert <victor.tolbert@gmail.com>
2019-03-16 09:29:36 -0400

    chore: add some prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,16 @@ module.exports = {
     'plugin:vue/recommended'
   ],
   rules: {
-    'no-unused-vars': 2
+    'no-unused-vars': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        semi: false,
+        printWidth: 120,
+        tabWidth: 2,
+      }
+    ]
   }
 }
